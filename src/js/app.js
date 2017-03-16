11a8840a9f8d28e706b375cb69b3242e94748f55
anlor <andreaslorentsson99@gmail.com>
2017-03-16 23:08:41 +0100

    added js and sass

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import { TapeMachine } from 'modules/helpers';
+
+const tp = new TapeMachine();
+tp.record('Hello... Hellooooo!!! Helloooooo!!!!!');
+tp.play();
---
+++ src/js/modules/helpers.js
@@ -0,0 +1,13 @@
+export class TapeMachine {
+    constructor() {
+        this.recordedMessage = '';
+    }
+
+    record(message) {
+        this.recordedMessage = message;
+    }
+
+    play() {
+        console.log(this.recordedMessage);
+    }
+}
